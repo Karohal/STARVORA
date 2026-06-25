becbd3e6a85d103d9e38a2ec3b7fd8d9653d566a
Stephane <124917972+Karohal@users.noreply.github.com>
2026-06-25 20:18:03 +0200

    v144: vitesse route /2 -> 0.075

--- js/engine/logistics.js
+++ js/engine/logistics.js
@@ -323,7 +323,7 @@ function updateTrucks(timestamp) {
   }
 }
 
-const TRUCK_SPEED_ROUTE   = 0.15;
+const TRUCK_SPEED_ROUTE   = 0.075;
 const TRUCK_SPEED_NOROUTE = 0.025;
 
 function getRoadLevel(col, row) {
